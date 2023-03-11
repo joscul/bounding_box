43a2d3ba42fc4616e83046bc041675e9b05542f6
bubbarubba <34864390+bubbarubba@users.noreply.github.com>
2023-03-11 13:46:35 +0100

    Davids första commit.

--- bounding_box.py
+++ bounding_box.py
@@ -1,15 +1,22 @@
 
 class bounding_box:
-    x = 0
-    y = 0
-    width = 0
-    height = 0
+	x = 0
+	y = 0
+	width = 0
+	height = 0
+	z = 0
+	def __init__(self, x, y, width, height):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
 
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+	def area(self):
+		return self.width * self.height
 
-    def area(self):
-        return self.width * self.height
+	def omkrets(self):
+		return 0
+
+	def overlaps(self, other_box):
+
+		return False
--- main.py
+++ main.py
@@ -5,4 +5,8 @@ box1 = bounding_box(0, 0, 10, 10)
 
 print(box1.area())
 
+box2 = bounding_box(0, 0, 20, 20)
+
+print(box2.area())
+
 
